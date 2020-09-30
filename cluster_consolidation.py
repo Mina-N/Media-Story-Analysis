1821b56aed884f7f1acc2685641a3c64a1875e78
unknown <minamjn@gmail.com>
2020-09-30 11:45:29 -0400

    initial changes for IE

--- cluster_consolidation.py
+++ cluster_consolidation.py
@@ -1,41 +1,47 @@
 import pandas as pd
 import numpy as np
 import re
+import pickle
+import csv
 
-topic_file = "../ht_topics.csv"
-topic_names = ["T" + str(i) for i in range(1, 20)]
-topic_values = [np.float64 for i in range(1, 20)]
+
+topic_file = "../ie_topics.csv"
+topic_names = ["T" + str(i) for i in range(1, 21)]
+topic_values = [np.float64 for i in range(1, 21)]
 topic_dict = {k : v for k, v in zip(topic_names, topic_values)}
 topic_dict["url"] = str
-topic_dict["article_text"] = str
+topic_dict["text"] = str
 topic_dict["Topic1"] = str
 topic_dict["Topic2"] = str
-col_names = ["url", "article_text", "Topic1", "Topic2"] + topic_names
+col_names = ["url", "text", "Topic1", "Topic2"] + topic_names
 topics = pd.read_csv(topic_file, usecols = col_names, dtype = topic_dict)
 
-ht_directory_1 = "../ht/ht1-6monthsscrapedinfo.csv"
-ht_directory_2 = "../ht/ht7-12monthsscrapedinfo.csv"
-ht1 = pd.read_csv(ht_directory_1, usecols = ["article_text", "url"])
-ht2 = pd.read_csv(ht_directory_2, usecols = ["article_text", "url"])
+ht_directory_1 = "../ie/ie1-6monthsscrapedinfo.csv"
+ht_directory_2 = "../ie/ie7-12monthsscrapedinfo.csv"
+ht1 = pd.read_csv(ht_directory_1, usecols = ["text", "url"])
+ht2 = pd.read_csv(ht_directory_2, usecols = ["text", "url"])
 
 print(ht1.shape[0])
 print(ht2.shape[0])
 
 topic_text = []
-temp_df = topics.loc[:, 'article_text']
+temp_df = topics.loc[:, 'text']
 for i in range(temp_df.shape[0]):
     topic_text.append(temp_df.iloc[i])
 
 topic_vals = []
-temp_df = topics.loc[:, 'Topic1' : 'T19']
+temp_df = topics.loc[:, 'Topic1' : 'T20']
 for i in range(temp_df.shape[0]):
     topic_vals.append(list(temp_df.iloc[i, :]))
 
+# topics_dict contains topic text and topic dist from combined file
 topics_dict = {k : v for k, v in zip(topic_text, topic_vals)}
 
 count = 0
+count1 = 0
+count2 = 0
 for i in range(ht1.shape[0]):
-    text = ht1.loc[i, "article_text"]
+    text = ht1.loc[i, "text"]
     if ((isinstance(text, float) == False) and (len(text) != 0)):
         text = text.replace("\n", " ")
         text = text.replace("\r", " ")
@@ -46,18 +52,22 @@ for i in range(ht1.shape[0]):
         text = re.sub("\\s+", " ", text)
         text = text.strip()
 
-        # 9 ARTICLES THAT WERE NOT FOUND
         if (text in topics_dict.keys()):
             count += 1
             topic_list = topics_dict[text]
             topic_list.append(ht1.loc[i, "url"])
             topics_dict[text] = topic_list
+        else:
+            count1 += 1
+    else:
+        count2 += 1
 
-print(count)
 
 count = 0
+count1 = 0
+count2 = 0
 for i in range(ht2.shape[0]):
-    text = ht2.loc[i, "article_text"]
+    text = ht2.loc[i, "text"]
     if ((isinstance(text, float) == False) and (len(text) != 0)):
         text = text.replace("\n", " ")
         text = text.replace("\r", " ")
@@ -73,51 +83,61 @@ for i in range(ht2.shape[0]):
             topic_list = topics_dict[text]
             topic_list.append(ht2.loc[i, "url"])
             topics_dict[text] = topic_list
+        else:
+            count1 += 1
+    else:
+        count2 += 1
+
+
+new_topics_dict = {}
+for key, val in topics_dict.items():
+    old_key = key
+    old_val = val
+    new_val = old_val[0:22] # 22 for IE, 21 for HT
+    new_key = old_val[22:]
+    #(new_val)
+    #print(new_key)
+    for i in new_key:
+        new_topics_dict[i] = new_val
+
+col_type = {"article_title": str, "URL": str, "cluster_id" : int}
+combined = []
+for i in range(1, 13):
+    for j in range(1, 4):
+        combined.append(pd.read_csv("../ie/ie_combined_mo" + str(i) + "p" + str(j) + ".csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type))
+
+combined = pd.concat(combined, ignore_index = True)
+
+combined["Topic1"] = ""
+combined["Topic2"] = ""
+for i in range(1, 21):
+    combined["T" + str(i)] = 0
+
+print(combined.head())
+unmatched_urls = []
+count = 0
+for i in range(combined.shape[0]):
+    if ((str(combined.loc[i, "URL"]) + "\n") in new_topics_dict.keys()):
+        top_list = new_topics_dict[combined.loc[i, "URL"] + "\n"]
+        combined.loc[i, "Topic1" : "T20"] = top_list
+    elif (str(combined.loc[i, "URL"]) in new_topics_dict.keys()):
+        top_list = new_topics_dict[combined.loc[i, "URL"]]
+        combined.loc[i, "Topic1": "T20"] = top_list
+        print("GOT HERE")
+    else:
+        count += 1
+        unmatched_urls.append(combined.loc[i, "URL"])
+        print("OH NOOOOOOOO")
 
 print(count)
+print(combined.head())
+combined.to_csv("ie_combined.csv")
 
-#
-# new_topics_dict = {}
-# for key, val in topics_dict.items():
-#     old_key = key
-#     old_val = val
-#     new_val = old_val[0:21]
-#     new_key = old_val[21:]
-#     print(new_val)
-#     print(new_key)
-#     for i in new_key:
-#         new_topics_dict[i] = new_val
+with open('unmatched_urls.csv','w') as result_file:
+    wr = csv.writer(result_file, dialect='excel')
+    wr.writerows(unmatched_urls)
 
-# col_type = {"article_title": str, "URL": str, "cluster_id" : int}
-# combined1 = pd.read_csv("../ht/ht_combined_mo1.csv", usecols = ["article_title", "URL", "cluster_id"] , dtype = col_type)
-# combined2 = pd.read_csv("../ht/ht_combined_mo2.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined3 = pd.read_csv("../ht/ht_combined_mo3.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined4 = pd.read_csv("../ht/ht_combined_mo4.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined5 = pd.read_csv("../ht/ht_combined_mo5.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined6 = pd.read_csv("../ht/ht_combined_mo6.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined7 = pd.read_csv("../ht/ht_combined_mo7.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined8 = pd.read_csv("../ht/ht_combined_mo8.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined9 = pd.read_csv("../ht/ht_combined_mo9.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined10 = pd.read_csv("../ht/ht_combined_mo10.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined11 = pd.read_csv("../ht/ht_combined_mo11.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-# combined12 = pd.read_csv("../ht/ht_combined_mo12.csv", usecols = ["article_title", "URL", "cluster_id"], dtype = col_type)
-
-# combined = pd.concat([combined1, combined2, combined3, combined4, combined5, combined6,
-#                       combined7, combined8, combined9, combined10, combined11, combined12],
-#                      ignore_index = True)
-#
-# combined["Topic1"] = ""
-# combined["Topic2"] = ""
-# for i in range(1, 20):
-#     combined["T" + str(i)] = 0
-#
-# for i in range(combined.shape[0]):
-#     if (combined.loc[i, "URL"] in new_topics_dict.keys()):
-#         top_list = new_topics_dict[combined.loc[i, "URL"]]
-#         combined.loc[i, "Topic1" : "T19"] = top_list
-#
-# print(combined.head())
-# combined.to_csv("ht_combined.csv")
+exit(1)
 
 topic_names = ["article_title", "URL", "cluster_id", "Topic1", "Topic2"]
 topic_dist = ["T" + str(i) for i in range(1, 20)]
@@ -125,128 +145,59 @@ topic_names = topic_names + topic_dist
 topic_values = [str, str, int, str, str]
 topic_values = topic_values + [np.float64 for i in range(1, 20)]
 col_dict = {k : v for k, v in zip(topic_names, topic_values)}
-
 combined = pd.read_csv("ht_combined.csv", usecols = topic_names , dtype = col_dict)
 
-# Split combined dataframe into 12 separate dataframes
-ht_combined = []
-start_index = 0
-for i in range(1, combined.shape[0]):
-    if (i == (combined.shape[0] - 1)):
-        ht_combined.append(combined.iloc[start_index: i, :])
-    if (combined.loc[i, "cluster_id"] == 0 and combined.loc[i - 1, "cluster_id"] != 0):
-        ht_combined.append(combined.iloc[start_index : i, :])
-        start_index = i
+combined_dict = {}
+for i in range(combined.shape[0]):
+        combined_dict[combined.loc[i, "URL"]] = [combined.loc[i, "Topic1"], combined.loc[i, "Topic2"],
+                                combined.loc[i, "T1"], combined.loc[i, "T2"], combined.loc[i, "T3"], combined.loc[i, "T4"],
+                               combined.loc[i, "T5"], combined.loc[i, "T6"], combined.loc[i, "T7"], combined.loc[i, "T8"],
+                               combined.loc[i, "T9"], combined.loc[i, "T10"], combined.loc[i, "T11"], combined.loc[i, "T12"],
+                               combined.loc[i, "T13"], combined.loc[i, "T14"], combined.loc[i, "T15"], combined.loc[i, "T16"],
+                               combined.loc[i, "T17"], combined.loc[i, "T18"], combined.loc[i, "T19"]]
 
-for data in ht_combined:
-    data.index = np.arange(len(data))
+topic_names = ["storytime", "id", "url", "cluster_id", "lmviews", "seq"]
+topic_values = [str, int, str, int, np.float64, int]
+col_dict = {k : v for k, v in zip(topic_names, topic_values)}
+mina = pd.read_csv("../mina_IE.csv", usecols = topic_names, dtype = col_dict)
 
-orig_dict = {}
-original_file = ht_combined[0]
-for i in range(original_file.shape[0]):
-    cluster_id = original_file.loc[i, "cluster_id"]
-    if cluster_id in orig_dict.keys():
-        url_list = orig_dict[cluster_id]
+mina_dict = {}
+for i in range(mina.shape[0]):
+    cluster_id = mina.loc[i, "cluster_id"]
+    if cluster_id in mina_dict.keys():
+        print("got here")
+        dict_list = mina_dict[cluster_id]
         url_dict = {}
-        url_dict[original_file.loc[i, "URL"]] = [original_file.loc[i, "Topic1"], original_file.loc[i, "Topic2"],
-                                original_file.loc[i, "T1"], original_file.loc[i, "T2"], original_file.loc[i, "T3"], original_file.loc[i, "T4"],
-                               original_file.loc[i, "T5"], original_file.loc[i, "T6"], original_file.loc[i, "T7"], original_file.loc[i, "T8"],
-                               original_file.loc[i, "T9"], original_file.loc[i, "T10"], original_file.loc[i, "T11"], original_file.loc[i, "T12"],
-                               original_file.loc[i, "T13"], original_file.loc[i, "T14"], original_file.loc[i, "T15"], original_file.loc[i, "T16"],
-                               original_file.loc[i, "T17"], original_file.loc[i, "T18"], original_file.loc[i, "T19"]]
-        url_list.append(url_dict)
-        orig_dict[cluster_id] = url_list
+        top_list = combined_dict[mina.loc[i, "url"]]
+        url_dict[mina.loc[i, "url"]] = top_list
+        dict_list.append(url_dict)
+        mina_dict[cluster_id] = dict_list
     else:
         url_dict = {}
-        url_dict[original_file.loc[i, "URL"]] = [original_file.loc[i, "Topic1"], original_file.loc[i, "Topic2"],
-                               original_file.loc[i, "T1"], original_file.loc[i, "T2"], original_file.loc[i, "T3"], original_file.loc[i, "T4"],
-                               original_file.loc[i, "T5"], original_file.loc[i, "T6"], original_file.loc[i, "T7"], original_file.loc[i, "T8"],
-                               original_file.loc[i, "T9"], original_file.loc[i, "T10"], original_file.loc[i, "T11"], original_file.loc[i, "T12"],
-                               original_file.loc[i, "T13"], original_file.loc[i, "T14"], original_file.loc[i, "T15"], original_file.loc[i, "T16"],
-                               original_file.loc[i, "T17"], original_file.loc[i, "T18"], original_file.loc[i, "T19"]]
-        orig_dict[cluster_id] = [url_dict]
+        top_list = combined_dict[mina.loc[i, "url"]]
+        url_dict[mina.loc[i, "url"]] = top_list
+        mina_dict[cluster_id] = [url_dict]
+
+print(len(mina_dict))
+
+def save_obj(obj, name ):
+    with open('obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+save_obj(mina_dict, "mina_dict_ie")
 
 
-cluster_id_counter = len(orig_dict)
-for ht_index in range(1, len(ht_combined)):
-    print(ht_index)
-    adjacent_file = ht_combined[ht_index]
-    adjacent_dict = {}
-    for i in range(adjacent_file.shape[0]):
-        cluster_id = adjacent_file.loc[i, "cluster_id"]
-        if cluster_id in adjacent_dict.keys():
-            url_list = adjacent_dict[cluster_id]
-            url_dict = {}
-            url_dict[adjacent_file.loc[i, "URL"]] = [adjacent_file.loc[i, "Topic1"], adjacent_file.loc[i, "Topic2"],
-                                                     adjacent_file.loc[i, "T1"], adjacent_file.loc[i, "T2"],
-                                                     adjacent_file.loc[i, "T3"], adjacent_file.loc[i, "T4"],
-                                                     adjacent_file.loc[i, "T5"], adjacent_file.loc[i, "T6"],
-                                                     adjacent_file.loc[i, "T7"], adjacent_file.loc[i, "T8"],
-                                                     adjacent_file.loc[i, "T9"], adjacent_file.loc[i, "T10"],
-                                                     adjacent_file.loc[i, "T11"], adjacent_file.loc[i, "T12"],
-                                                     adjacent_file.loc[i, "T13"], adjacent_file.loc[i, "T14"],
-                                                     adjacent_file.loc[i, "T15"], adjacent_file.loc[i, "T16"],
-                                                     adjacent_file.loc[i, "T17"], adjacent_file.loc[i, "T18"],
-                                                     adjacent_file.loc[i, "T19"]]
-            url_list.append(url_dict)
-            adjacent_dict[cluster_id] = url_list
-        else:
-            url_dict = {}
-            url_dict[adjacent_file.loc[i, "URL"]] = [adjacent_file.loc[i, "Topic1"], adjacent_file.loc[i, "Topic2"],
-                                                     adjacent_file.loc[i, "T1"], adjacent_file.loc[i, "T2"],
-                                                     adjacent_file.loc[i, "T3"], adjacent_file.loc[i, "T4"],
-                                                     adjacent_file.loc[i, "T5"], adjacent_file.loc[i, "T6"],
-                                                     adjacent_file.loc[i, "T7"], adjacent_file.loc[i, "T8"],
-                                                     adjacent_file.loc[i, "T9"], adjacent_file.loc[i, "T10"],
-                                                     adjacent_file.loc[i, "T11"], adjacent_file.loc[i, "T12"],
-                                                     adjacent_file.loc[i, "T13"], adjacent_file.loc[i, "T14"],
-                                                     adjacent_file.loc[i, "T15"], adjacent_file.loc[i, "T16"],
-                                                     adjacent_file.loc[i, "T17"], adjacent_file.loc[i, "T18"],
-                                                     adjacent_file.loc[i, "T19"]]
-            adjacent_dict[cluster_id] = [url_dict]
 
-    print("created adjacent dict")
 
-    for key, value in adjacent_dict.items():
-        flag = 0
-        url_value = []
-        for i in value:
-            url_value += i.keys()
-        for key2, value2 in orig_dict.items():
-            url_value2 = []
-            for i in value2:
-                url_value2 += i.keys()
-            #print(url_value2)
-            #print(url_value)
-            if (not set(url_value).isdisjoint(url_value2)):
-                #print("match")
-                no_dup_list = url_value + url_value2
-                no_dup_list = list(set(no_dup_list))
-                for url in no_dup_list:
-                    if url in url_value:
-                        url_index = url_value.index(url)
-                        orig_list = orig_dict[key2]
-                        orig_list.append(value[url_index])
-                        orig_dict[key2] = orig_list
-                    else:
-                        url_index = url_value2.index(url)
-                        orig_list = orig_dict[key2]
-                        orig_list.append(value2[url_index])
-                        orig_dict[key2] = orig_list
-                flag = 1
-                break
-        if (flag == 0):
-            #print("no match")
-            #print(value)
-            orig_dict[cluster_id_counter] = value
-            cluster_id_counter += 1
 
-    print("compared adjacent dict to original dict")
 
-print(len(orig_dict))
-for key, val in orig_dict.items():
-    print(key)
-    print(val)
+
+
+
+
+
+
+
 
 
 
