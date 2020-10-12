b031121c086fcd4ae98fb38228270656bb110b5a
unknown <minamjn@gmail.com>
2020-10-12 11:56:13 -0400

    customized cluster visualizations for ie

--- cluster_vis.py
+++ cluster_vis.py
@@ -17,13 +17,13 @@ def save_obj(obj, name ):
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 max_avg_topic = []
-mina_dict = load_obj("mina_dict_ht")
+mina_dict = load_obj("mina_dict_ie_results")
 for key, value in mina_dict.items(): # value is a list of dictionaries
     value_length = len(value)
-    avg_topic_dist = [0 for i in range(0, 19)]
-    for elem in value: # elem is a dictionary with a URL key and topic distribution value
-        for key2, value2 in elem.items(): # value2 is a list of top two topics and topic percentages
-            value2 = value2[2:]
+    avg_topic_dist = [0 for i in range(0, 19)] # 19 0's
+    for elem in value: # elem is a dictionary with an id key and topic distribution value
+        for key2, value2 in elem.items(): # value2 is a list of top two topics, topic percentages, and seq
+            value2 = value2[2:-1]
             avg_topic_dist = np.array([a + b for a, b in zip(avg_topic_dist, value2)], dtype = 'f')
             #print(avg_topic_dist)
     avg_topic_dist = avg_topic_dist / value_length
@@ -32,6 +32,7 @@ for key, value in mina_dict.items(): # value is a list of dictionaries
         max_avg_topic.append([key, max_index + 1])
 
 print(len(max_avg_topic))
+# Prints out groupings of cluster id, topic number
 print(max_avg_topic[0:5])
 
 # Create side-by-side bar plots of topic percentages
@@ -39,24 +40,23 @@ print(max_avg_topic[0:5])
 # Pass the x and y coordinates of the bars to the
 # function. The label argument gives a label to the data.
 for i in range(0, 2):
-    list_dict = mina_dict[max_avg_topic[i][0]]
-    index = 1
+    list_dict = mina_dict[max_avg_topic[i][0]] # identify topic lists of first two clusters in max_avg_topic
     for dict in list_dict:
         value = []
-        for key in dict:
+        for key in dict.keys():
             value = dict[key]
         print(value)
-        plt.bar([i for i in range(1, 20)], value[2:])
+        plt.bar([i for i in range(1, 20)], value[2:-1])
         plt.legend()
-
+        article_num = value[-1]
         # The following commands add labels to our figure.
         plt.xlabel('Topic Number')
         plt.ylabel('Topic Percentage')
-        plt.title('Topic Percentage Distribution for Cluster ' + str(max_avg_topic[i][0]) + ' and Article ' + str(index))
+        plt.title('Topic Percentage Distribution for Cluster ' + str(max_avg_topic[i][0]) + ' and Article ' + str(article_num))
 
         plt.show()
-        index += 1
 
+exit(1)
 # Sentiment analysis visualizations
 # Read in sentiment analysis csv file where urls are associated with sentiment scores
 
